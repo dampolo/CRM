df3784c16cd5d2e7ab5436e1fd38b31078f91e4a
Damian Poloczek <damianpoloczek@gmail.com>
2024-08-14 13:54:17 +0200

    dialog implemented

--- src/app/dialog-add-user/dialog-add-user.component.ts
+++ src/app/dialog-add-user/dialog-add-user.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatDialogContent, MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-dialog-add-user',
   standalone: true,
-  imports: [],
+  imports: [MatDialogContent, MatFormFieldModule, MatInputModule, FormsModule, MatDialogModule],
   templateUrl: './dialog-add-user.component.html',
-  styleUrl: './dialog-add-user.component.scss'
+  styleUrl: './dialog-add-user.component.scss',
 })
-export class DialogAddUserComponent {
-
-}
+export class DialogAddUserComponent {}
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -3,12 +3,29 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatButtonModule } from '@angular/material/button';
 import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.component';
+
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [MatButtonModule, MatDividerModule, MatIconModule, MatTooltipModule],
+  imports: [
+    MatButtonModule,
+    MatDividerModule,
+    MatIconModule,
+    MatTooltipModule,
+    MatDialogModule,
+    DialogAddUserComponent
+  ],
   templateUrl: './user.component.html',
   styleUrl: './user.component.scss',
 })
-export class UserComponent {}
+export class UserComponent {
+
+  constructor(public dialog: MatDialog) {}
+
+  openDialog(): void {
+    this.dialog.open(DialogAddUserComponent);
+  }
+}
